f497e80149528bb6408ef67d4bd25ab2cc086dae
DanWe <weikert-deluxe@gmx.de>
2024-08-31 10:42:42 +0200

    Update2 o.py

--- o.py
+++ o.py
@@ -1,7 +1,7 @@
 import ollama
 import streamlit as st
 
-prompt = "List 5 great Consumer Staple stocks to invest in which are not mainstream."
+prompt = "List 10 great Consumer Staple stocks to invest in which are not mainstream."
 
 chat = ollama.chat(
     model="llama3.1:8b", messages=[{"role": "user", "content": prompt}], stream=True
